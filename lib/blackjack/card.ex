1ed9fe6f29369f3ee774d4f701207442cc3bc50c
Aaron Bruckner <aaronbruckner@gmail.com>
2023-06-28 21:53:44 -0700

    Added basic card type

---
+++ lib/blackjack/card.ex
@@ -0,0 +1,27 @@
+defmodule Blackjack.Card do
+
+  @typedoc """
+  Defines the four allowed suit types a card may be.
+  """
+  @type suit() :: :heart | :diamond | :spade | :club
+
+  @typedoc """
+  Defines the allowed value of a card (2, 10, Jack, Ace, etc)
+  """
+  @type value() :: 2..10 | :jack | :queen | :king | :ace
+
+  @type t() :: %__MODULE__{
+    suit: suit(),
+    value: value(),
+  }
+
+  @enforce_keys [:suit, :value]
+  defstruct [:suit, :value]
+end
+
+defimpl String.Chars, for: Blackjack.Card do
+  @spec to_string(Blackjack.Card.t()) :: String.t()
+  def to_string(card) do
+    "#{card.value} of #{card.suit}s"
+  end
+end
---
+++ test/blackjack/card_test.exs
@@ -0,0 +1,13 @@
+defmodule BlackjackCardTest do
+  use ExUnit.Case
+  doctest Blackjack.Card
+
+  test "struct - implements to_string for numeric value" do
+    assert "#{%Blackjack.Card{suit: :heart, value: 2}}" === "2 of hearts"
+  end
+
+  test "struct - implements to_string for face value" do
+    assert "#{%Blackjack.Card{suit: :spade, value: :king}}" === "king of spades"
+  end
+
+end
