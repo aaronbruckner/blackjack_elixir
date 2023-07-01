b3ae147158b392af8aefd3099e38eb001496e221
Aaron Bruckner <aaronbruckner@gmail.com>
2023-07-01 08:58:34 -0700

    Added ability to build custom deck with cards in specific order

--- lib/blackjack/deck.ex
+++ lib/blackjack/deck.ex
@@ -1,23 +1,34 @@
 defmodule Blackjack.Deck do
-  @opaque t() :: list(%Blackjack.Card{})
+  alias Blackjack.Card
+  @opaque t() :: list(Card.t())
 
   @doc """
   Returns a shuffled standard deck of 52 cards across the four suits and range of values (2 through Ace).
   All cards are unique value/suit pairs.
   """
+  @spec new() :: t()
   def new() do
     for value <- [2, 3, 4, 5, 6, 7, 8, 9, 10, :jack, :queen, :king, :ace],
         suit <- [:heart, :diamond, :spade, :club] do
-      %Blackjack.Card{value: value, suit: suit}
+      Card.new(suit, value)
     end
     |> Enum.shuffle()
+  end
+
+  @doc """
+  Builds a deck with the specific cards provided in the order they are provided.
+  Cards earlier in the list will be pulled first.
+  """
+  @spec new(list(Card.t())) :: t()
+  def new(cards) do
+    cards
   end
 
   @doc """
   Pulls the top card from the deck, returning the card and the new deck without the top card.
   If the deck is empty, the returned card will be nil.
   """
-  @spec pull_top_card(t()) :: {%Blackjack.Card{} | nil, t()}
+  @spec pull_top_card(t()) :: {%Card{} | nil, t()}
   def pull_top_card([card | remainingDeck]) do
     {card, remainingDeck}
   end
--- test/blackjack/deck_test.exs
+++ test/blackjack/deck_test.exs
@@ -1,36 +1,51 @@
 defmodule BlackjackDeckTest do
   use ExUnit.Case
 
+  alias Blackjack.Card
+  alias Blackjack.Deck
+
   @max_cards_in_deck 52
 
   test "new - should shuffle the deck" do
     # Would be exceptionally rare for this to ever fail with 52 cards.
-    assert Blackjack.Deck.new() !== Blackjack.Deck.new()
+    assert Deck.new() !== Deck.new()
+  end
+
+  test "new - with specific cards" do
+    card1 = Card.new(:heart, 2)
+    card2 = Card.new(:heart, 3)
+    card3 = Card.new(:heart, 4)
+
+    deck = Deck.new([card1, card2, card3])
+
+    assert {^card1, deck} = Deck.pull_top_card(deck)
+    assert {^card2, deck} = Deck.pull_top_card(deck)
+    assert {^card3, deck} = Deck.pull_top_card(deck)
   end
 
   test "pull_top_card - returns 52 cards" do
-    deck = Blackjack.Deck.new()
+    deck = Deck.new()
 
     Enum.reduce(1..@max_cards_in_deck, deck, fn _element, deck ->
-      assert {%Blackjack.Card{value: _, suit: _}, nextDeck} = Blackjack.Deck.pull_top_card(deck)
+      assert {_, nextDeck} = Deck.pull_top_card(deck)
       nextDeck
     end)
   end
 
   test "pull_top_card - returns no card on 53rd pull" do
     deck =
-      Enum.reduce(1..@max_cards_in_deck, Blackjack.Deck.new(), fn _element, deck ->
-        {_, nextDeck} = Blackjack.Deck.pull_top_card(deck)
+      Enum.reduce(1..@max_cards_in_deck, Deck.new(), fn _element, deck ->
+        {_, nextDeck} = Deck.pull_top_card(deck)
         nextDeck
       end)
 
-    assert {nil, _} = Blackjack.Deck.pull_top_card(deck)
+    assert {nil, _} = Deck.pull_top_card(deck)
   end
 
   test "pull_top_card - returns unique cards" do
-    Enum.reduce(1..@max_cards_in_deck, {Blackjack.Deck.new(), MapSet.new()}, fn _,
+    Enum.reduce(1..@max_cards_in_deck, {Deck.new(), MapSet.new()}, fn _,
                                                                                 {deck, seenCards} ->
-      {card, nextDeck} = Blackjack.Deck.pull_top_card(deck)
+      {card, nextDeck} = Deck.pull_top_card(deck)
       cardKey = "#{card}"
 
       case MapSet.member?(seenCards, cardKey) do
